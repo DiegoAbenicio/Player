3d299c176efba4262d5a32f974c2625f7fc329bd
DiegoAbenicio <diego.abenicio1@gmail.com>
2022-01-18 10:10:30 -0300

    Corrige Cadastro

--- cadastro.php
+++ cadastro.php
@@ -2,39 +2,32 @@
 
 $login = $_POST['login'];
 $senha = MD5($_POST['senha']);
+$senha2= $_POST['senha'];
+/*Faz a conexão com o banco de daos, as informações 'localhost', 'root' e 'null' 
+  devem ser trocadas pelas informações pessoais da sua máquina */
 $connect = mysqli_connect('localhost', 'root', null);
 $db = mysqli_select_db($connect, 'songlib');
 $query_select = "SELECT login FROM usuarios WHERE login = '$login'";
 $select = mysqli_query($connect, $query_select);
-$array = mysqli_fetch_array($select);
-$logarray = $array['login'];
 
-  if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>
-    alert('O campo login deve ser preenchido');window.location.href='
-    cadastro.html';</script>";
 
-    }else{
-      if($logarray == $login){
-
-        echo"<script language='javascript' type='text/javascript'>
-        alert('Esse login já existe');window.location.href='
-        cadastro.html';</script>";
-        die();
-
-      }else{
-        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
-        $insert = mysqli_query($connect, $query);
-
-        if($insert){
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='index.html'</script>";
-        }else{
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro.html'</script>";
-        }
+if($senha2 == '' || $senha2 == null){
+  echo "<script language='javascript' type='text/javascript'>alert('O campo senha deve ser preenchido');window.location.href='register.html';</script>";
+}
+elseif($login == '' || $login == null){
+  echo "<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='register.html';</script>";
+}
+else{
+  $verificarusuario = mysqli_query($connect, "SELECT*FROM usuarios WHERE login='$login'"); 
+  $verificar = mysqli_fetch_array($verificarusuario);
+  if($verificar > 1){
+    $senha2 == null;
+    echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='register.html';</script>"; die();
+  }
+  else{
+    mysqli_query($connect, "INSERT INTO usuarios (login, senha) VALUES ( '$login', '$senha')");
+    echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='index.html'</script>";
+      
+          }
       }
-    }
 ?>
